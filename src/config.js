818d289453d9a5f6ac7188c88d7aa00dda41b0e0
Matthew Herron <matthew.herron77@gmail.com>
2019-03-18 20:13:54 -0500

    New config

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 module.exports = {
   product: 'ETH-USD',
-  timeframe: '30s',
+  timeframe: '1s',
   credentials: {
     key: process.env.KEY,
     secret: process.env.SECRET,
     passphrase: process.env.PASSPHRASE,
   },
-  periods: [10, 100],
+  periods: [100, 1000],
   limitMargin: 1.005,
   stopMargin: 1.002,
-  size: 0.01
+  size: 1
 }
