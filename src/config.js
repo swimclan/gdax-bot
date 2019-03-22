db7c276068700e2e6d149319880c4845ffe12cd8
Matthew Herron <matthew.herron77@gmail.com>
2019-03-21 21:07:20 -0500

    Updated crossover timeframe

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 module.exports = {
   product: 'ETH-USD',
-  timeframe: '1s',
+  timeframe: '2s',
   credentials: {
     key: process.env.KEY,
     secret: process.env.SECRET,
     passphrase: process.env.PASSPHRASE,
   },
-  periods: [10, 20],
+  periods: [10, 100],
   limitMargin: 1.0001,
   stopMargin: 1.0001,
   size: 5
