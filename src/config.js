23276d376c05e71d1ae962ef9d6b2b7ea0cf8150
Matthew Herron <matthew.herron77@gmail.com>
2019-03-24 20:35:41 -0500

    Change default config

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 module.exports = {
   product: 'ETH-USD',
-  timeframe: '20m',
+  timeframe: '1m',
   credentials: {
     key: process.env.KEY,
     secret: process.env.SECRET,
     passphrase: process.env.PASSPHRASE,
   },
-  periods: [500, 1000],
+  periods: [20, 200],
   limitMargin: 1.01,
   stopMargin: 1.002,
   size: 5
